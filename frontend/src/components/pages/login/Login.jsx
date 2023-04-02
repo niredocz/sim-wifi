87c3e8ebc550e4526cb6d8ac319d2b6b6d2177f5
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-04-02 16:16:53 +0700

    layouting login page

--- frontend/src/components/pages/login/Login.jsx
+++ frontend/src/components/pages/login/Login.jsx
@@ -1,18 +1,83 @@
 import React from "react"
-import { CardLogin, FormLogin, LeftSection, LoginWrapper } from "./LoginStyled"
+import {
+  CardLogin,
+  FormLogin,
+  LeftSection,
+  LoginTitle,
+  LoginWrapper,
+  RightSection,
+  FormLabel,
+  FormWrapper,
+  InputText,
+  InputTextarea,
+  TosWrapper,
+  TosCheckbox,
+  SubmitButton,
+} from "./LoginStyled"
 
 function Login() {
   return (
     <LoginWrapper>
-      <LeftSection>
-        <h2>Login SIM WiFi Ngawi</h2>
-      </LeftSection>
       <CardLogin>
-        <FormLogin method="GET">
-          <div>
-            <input type="text" />
-          </div>
-        </FormLogin>
+        <LeftSection>
+          <LoginTitle>Login SIM WiFi Ngawi</LoginTitle>
+          <img src="/img-login.png" alt="img-login" />
+        </LeftSection>
+        <RightSection>
+          <FormLogin method="GET">
+            <h1 style={{ marginBottom: 20 }}>Daftar Akun</h1>
+
+            <div style={{ display: "flex", gap: 15, width: "100%" }}>
+              <FormWrapper>
+                <FormLabel htmlFor="first-name">Nama Depan</FormLabel>
+                <InputText id="first-name" type="text" />
+              </FormWrapper>
+              <FormWrapper>
+                <FormLabel htmlFor="last-name">Nama Belakang</FormLabel>
+                <InputText id="last-name" type="text" />
+              </FormWrapper>
+            </div>
+            <FormWrapper>
+              <FormLabel htmlFor="email">Alamat Email</FormLabel>
+              <InputText id="email" type="email" />
+            </FormWrapper>
+            <div style={{ display: "flex", gap: 15, width: "100%" }}>
+              <FormWrapper>
+                <FormLabel htmlFor="password">Kata Sandi</FormLabel>
+                <InputText id="password" type="password" />
+              </FormWrapper>
+              <FormWrapper>
+                <FormLabel htmlFor="repeat-password">
+                  Ulangi Kata Sandi
+                </FormLabel>
+                <InputText id="repeat-password" type="password" />
+              </FormWrapper>
+            </div>
+            <FormWrapper>
+              <FormLabel htmlFor="alamat">Alamat Rumah</FormLabel>
+              <InputTextarea name="" id="alamat" rows="10"></InputTextarea>
+            </FormWrapper>
+            <FormWrapper>
+              <FormLabel htmlFor="phone">No. HP</FormLabel>
+              <InputText id="phone" type="text" />
+            </FormWrapper>
+
+            <TosWrapper>
+              <TosCheckbox type="checkbox" name="" id="tos" />
+
+              <FormLabel htmlFor="tos" style={{ marginBottom: 0 }}>
+                saya bersedia untuk menyetujui{" "}
+                <a href="#">syarat dan ketentuan</a> yang berlaku.
+              </FormLabel>
+            </TosWrapper>
+
+            <p style={{ marginBottom: 10 }}>
+              Sudah memiliki akun ? <a href="#">Masuk disini.</a>
+            </p>
+
+            <SubmitButton type="submit">Daftarkan Akun</SubmitButton>
+          </FormLogin>
+        </RightSection>
       </CardLogin>
     </LoginWrapper>
   )

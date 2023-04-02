2bce1f4260ec8f5b11c1d882e24d578f3acc884b
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-04-02 15:04:19 +0700

    add style to login page

--- frontend/src/components/pages/login/LoginStyled.js
+++ frontend/src/components/pages/login/LoginStyled.js
@@ -1,20 +1,20 @@
 import styled from "styled-components"
 
 export const LoginWrapper = styled.div`
-  // background-color: blue;
-  min-height: 100vh;
-  // overflow: hidden;
-  gap: 30px;
-  padding: 30px;
   display: flex;
-  // justify-content: center;
-  // align-items: center;
+  min-height: 100vh;
+`
+
+export const CardLogin = styled.div`
+  display: flex;
+  gap: 30px;
+  margin: 30px;
+  width: 100%;
 `
 
 export const LeftSection = styled.div`
-  // height: 100%;
-  // min-height: 100vh;
-  min-width: 400px;
+  // min-width: 400px;
+  min-width: 30%;
   padding: 30px;
   color: #fff;
   display: flex;
@@ -23,11 +23,35 @@ export const LeftSection = styled.div`
   background-color: #3432f9;
 `
 
-export const CardLogin = styled.div`
+export const RightSection = styled.div`
+  width: 70%;
+  padding: 30px;
   background-color: #fff;
   border: 1px solid #e3e3e3;
-  max-width: 500px;
 `
+
+export const LoginTitle = styled.h2`
+  font-size: 24px;
+  font-weight: 500;
+  margin-top: 20px;
+`
+
 export const FormLogin = styled.form`
   display: block;
+`
+export const FormControl = styled.div`
+  display: grid;
+`
+
+export const FormLabel = styled.label`
+  display: block;
+  font-size: 18px;
+  font-weight: 700;
+  color: #2e444e;
+  margin-bottom: 10px;
+`
+
+export const FormWrapper = styled.div`
+  width: 100%;
+  margin-bottom: 15px;
 `
